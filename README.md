ac2f9ce2db6057e0503cc6baf894f5045b2cdad5
darshagarwal <64535202+darshagarwal@users.noreply.github.com>
2020-06-28 21:12:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# c-29project
-tower siege 1
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,22 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var block1,black2,blaock3,blaock4,block5,block6,block7,block8,block9;
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+    world = engine.world();
+
+  block1 = new block1(330,235,30,40);
+}
+
+function draw() {
+  Engine.update(engine);
+  background("black");  
+  block1.display();
+  drawSprites();
+}
